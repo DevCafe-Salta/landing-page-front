954c44eb733439fee3b57ff37e7ebd685e25cdc4
Agustin Piuca <aguspiucaquevedo@gmail.com>
2024-10-18 01:42:26 -0300

    Estructura de carpetas devCafe-Landing

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1 className="text-8xl font-bold underline">
+      ¡Hola mundo!
+    </h1>
+    </>
+  )
+}
+
+export default App
