c233790f2fdf17a597c4419dc2432a7b0076af09
ignacio-montaldi <ignaciomontaldi@gmail.com>
2024-10-25 09:59:53 -0300

    Añadido hook y context para cambiar entre tema claro y oscuro

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 // import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar/Navbar.tsx'
+import { ThemeProvider } from './context/themeContext';
 
 function App() {
 
   return (
     <>
+    <ThemeProvider>
     <Navbar/>
+    </ThemeProvider>
     </>
   )
 }
---
+++ src/context/themeContext.tsx
@@ -0,0 +1,22 @@
+import { createContext, useState } from "react";
+
+interface ThemeContextType {
+    theme: boolean;
+    setTheme: (value: boolean) => void;
+}
+
+interface ThemeProviderProps {
+    children: React.ReactNode;
+}
+
+export const ThemeContext = createContext<ThemeContextType|undefined>(undefined);
+
+export const ThemeProvider:React.FC<ThemeProviderProps> = ({children}) => {
+    // false = modo claro, true = modo oscuro.
+    const [theme, setTheme] = useState(false);
+    return (
+        <ThemeContext.Provider value={{theme, setTheme}}>
+            {children}
+        </ThemeContext.Provider>
+    )
+}
